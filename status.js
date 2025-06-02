c7ef3f4edc27336b2c71ca92dcbaeddcfc3c2187
trinketsbypickles <trinketsbypickles@gmail.com>
2025-06-02 01:26:43 -0700

    Add files via upload

---
+++ status.js
@@ -0,0 +1,38 @@
+// const liveStatus = {
+//     location: "Davis Street Market",
+//     status: "Selling",
+//     message: "I'm open! 🌸"
+// };
+
+
+// const liveStatus = {
+//     location: "Davis Street Market",
+//     status: "Setting up",
+//     message: "I'm getting the booth ready! Almost open — hang tight 🌞"
+// };
+
+
+// const liveStatus = {
+//     location: "Davis Street Market",
+//     status: "Packing",
+//     message: "I'm wrapping up for the day. Thanks to everyone who stopped by! ❤️"
+// };
+
+
+// const liveStatus = {
+//     location: "Preparing for Tomorrow’s Market",
+//     status: "Closed",
+//     message: "I'm off for the night but will be at the market tomorrow! Get excited 🌿"
+// };
+
+// const liveStatus = {
+//     location: "Home Studio",
+//     status: "Closed",
+//     message: "I can't be at the market tomorrow due to weather or personal reasons — check back soon! 💛"
+// };
+
+const liveStatus = {
+    location: "Currently Closed",
+    status: "Off",
+    message: "Thanks for stopping over the weekend! Prepping for the next Saturday market! 🌻"
+};
