20096a450061e93f9bfc2c3766de57c70d83b034
trinketsbypickles <trinketsbypickles@gmail.com>
2025-09-06 08:04:26 -0700

    Update status.js

--- status.js
+++ status.js
@@ -5,11 +5,11 @@
      //};
 
 
-// const liveStatus = {
-//     location: "Davis Street Market",
-//     status: "Setting up",
-//     message: "I'm getting the booth ready! Almost open — hang tight 🌞"
-// };
+ const liveStatus = {
+     location: "Davis Street Market",
+     status: "Setting up",
+     message: "I'm getting the booth ready! Almost open — hang tight 🌞"
+ };
 
 
 //const liveStatus = {
@@ -25,11 +25,11 @@
 //     message: "I'm off for the night but will be at the market tomorrow! Get excited 🌿"
 // };
 
- const liveStatus = {
-     location: "Closed",
-     status: "Off",
-     message: "Closed for the summer! Due to heat and small traffic at 3rd and B I will be taking a break! You can still DM for orders and pickups! Keep an eye out for some exciting new products and I'll see y'all on the first Saturday of September! 💛"
- };
+ // const liveStatus = {
+     // location: "Closed",
+     // status: "Off",
+     // message: "Closed for the summer! Due to heat and small traffic at 3rd and B I will be taking a break! You can still DM for orders and pickups! Keep an eye out for some exciting new products and I'll see y'all on the first Saturday of September! 💛"
+ // };
 
 //const liveStatus = {
     //location: "Currently Closed",
